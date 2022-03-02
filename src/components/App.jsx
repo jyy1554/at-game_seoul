891e9fc83bef1f9f41e86756fbd1fb664cd9fca7
jyy1554 <jyy1556@gmail.com>
2022-03-02 23:52:40 +0900

    지도 그리기 전 테스트 준비

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,15 @@ import Game from './Game/Game';
 import Header from './Header/Header';
 import Result from './Result/Result';
 import Welcome from './Welcome/Welcome';
+import Map from './Map/Map';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Welcome />} />
+          <Route path="/" element={<Map />} />
           <Route path="/game" element={<Game />} />
           <Route path="/result" element={<Result />} />
         </Routes>
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './css/index.css';
+
+function Map() {
+    return (
+        <div className="map">
+            <div className="wrapper">안녕하시렵니까</div>
+        </div>
+    );
+}
+
+export default Map;
