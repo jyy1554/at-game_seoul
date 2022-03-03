e4a7446af891bfd3987a37be617d9e85c2a5cdc4
jyy1554 <jyy1556@gmail.com>
2022-03-03 23:57:06 +0900

    d3 library 설치

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { json } from 'd3';
 import './css/index.css';
 
+const jsonUrl = 'assets/map/map_seoul.json';
+
 function Map() {
+    const [data, setData] = useState(null);
+
+    console.log(data);
+
+    useEffect(() => {
+        json(jsonUrl).then(setData);
+    }, []);
+
     return (
         <div className="map">
             <div className="wrapper">안녕하시렵니까</div>
