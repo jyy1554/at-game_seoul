b55a8b20c7626c01a22bf7327dd5a8c263297d40
jyy1554 <jyy1556@gmail.com>
2022-03-10 23:04:13 +0900

    지도에 라벨링 및 디자인

--- src/components/Game2/components/Map/Map.jsx
+++ src/components/Game2/components/Map/Map.jsx
@@ -27,8 +27,23 @@ function Map({ data }) {
       .selectAll(".gu")
       .data(data.features)    //gu라는 className을 가진 요소와 data 싱크를 맞춤
       .join("path")          //path라는 DOM element 요소를 추가
-      .attr("class", "gu")
-      .attr("d", feature => pathGenerator(feature));
+        .attr("class", "gu")
+        .attr("name", function(d){ return d.properties.name})
+        .attr("d", feature => pathGenerator(feature));
+
+    // Add the labels
+    svg
+      .selectAll(".labels")
+      .data(data.features)
+      .join("text")
+        .attr("class", "labels")
+        .attr("x", function(d){return pathGenerator.centroid(d)[0]})
+        .attr("y", function(d){return pathGenerator.centroid(d)[1]})
+        .text(function(d){ return d.properties.name})
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "central")
+        .style("font-size", 11)
+        .style("fill", "white");
     
   }, [data, dimensions]);
 
