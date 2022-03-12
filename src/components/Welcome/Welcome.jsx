185b26e0e1ad582cb5fae119146eae9652c780d9
jyy1554 <jyy1556@gmail.com>
2022-03-13 01:25:42 +0900

    welcome 문구 및 레이아웃 수정

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -21,7 +21,10 @@ function Welcome() {
         </span>
         <div className="title-container">
           <div className="main-title">at Game - 서울편</div>
-          <div className="sub-title">여기는 서울 어느 지역구에 속할까? 찐서울고수가 되어보세요! 😎</div>
+          <div className="subtitle-container">
+            <div className="subtitle">여기는 서울 어느 지역구에 속할까?</div>
+            <div className="subtitle">찐서울고수가 되어보세요! 😎</div>
+          </div>
           <div className="start-btn" onClick={__gameStart}>
             <button className='game-start'>게임시작</button>
           </div>
