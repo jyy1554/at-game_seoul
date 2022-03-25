ddb47b7cf872e58ca34e7485edec808d675ec546
jyy1554 <jyy1556@gmail.com>
2022-03-25 17:32:10 +0900

    새로고침 시 404에러 피하기 위해 HashRouter 사용

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from "./components/App";
 import { Provider } from 'react-redux';
 import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 ); 
